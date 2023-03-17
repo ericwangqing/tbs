3e92cf8f8c8c844afdc5dbea72c26191242bf50d
weihaopeng <Weihp16@gmail.com>
2023-03-17 22:15:33 +0800

    home page chain info

---
+++ src/pages/Home/components/WorldMap.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.world-map-container
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'WorldMap',
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
