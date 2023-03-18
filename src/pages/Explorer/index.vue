676f97be63944a5aa0e11a3399253b3977325d6b
weihaopeng <Weihp16@gmail.com>
2023-03-18 22:17:33 +0800

    add explorer and shard dir

---
+++ src/pages/Explorer/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.explorer
+  span TBS-testbed block explorer
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+name: 'Explorer',
+components: {},
+
+setup: () => {
+  return {}
+},
+})
+</script>
+
+<style scoped lang="scss">
+.explorer {
+  height: 100%;
+  width: 100%;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  span {
+    font-size: 36px;
+    margin-top: -60px;
+  }
+}
+</style>
