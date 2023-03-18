85acbae71b69c0475e544c8e2dca6d7e853bd76e
weihaopeng <Weihp16@gmail.com>
2023-03-18 22:17:33 +0800

    add explorer and shard dir

---
+++ src/pages/Shard/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.shard
+  span TBS-testbed shard chain list
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+name: 'Shard',
+components: {},
+
+setup: () => {
+  return {}
+},
+})
+</script>
+
+<style scoped lang="scss">
+.shard {
+  height: 100%;
+  width: 100%;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  span {
+    font-size: 36px;
+    margin-top: -60px;
+  }
+}
+</style>
