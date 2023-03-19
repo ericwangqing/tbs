e73928fb93c6017edda37676020db3a80031b1d0
weihaopeng <Weihp16@gmail.com>
2023-03-19 11:34:22 +0800

    basic star field, star move speed sync to the road line speed. When mouse down, speed up and mouse up, speed down.

--- src/pages/Benchmark/components/road-line.vue
+++ src/pages/Benchmark/components/road-line.vue
@@ -85,9 +85,15 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.road-line-container, canvas {
+.road-line-container {
+  position: absolute;
+  top: 0;
   width: 100%;
   height: 100%;
+  canvas {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
   
