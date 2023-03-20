f2adff688f80de6aa054d54b587a335c14c694d2
weihaopeng <Weihp16@gmail.com>
2023-03-20 20:24:43 +0800

    import iconfont; car trigger in road map change to red when tps is larger than 100k; enhance page layout.

--- src/pages/Benchmark/components/road-map.vue
+++ src/pages/Benchmark/components/road-map.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .road-map-container
-  svg(width="238px" height="229px" viewBox="-20 -20 238 229" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="filter: drop-shadow(0px 0px 4px 0px rgba(0, 0, 0, 0.3));")
+  svg(width="218px" height="209px" viewBox="-10 -10 218 209" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="filter: drop-shadow(0px 0px 4px 0px rgba(0, 0, 0, 0.3));")
     defs
     linearGradient(x1="0" y1="0" x2="0" y2="1" id="gradient1")
       stop(offset="0%" stop-color="#FFCB00")
@@ -11,22 +11,22 @@
       polygon(:points="points" stroke="#fff" stroke-width="5" fill="none" fill-rule="evenodd" :stroke-dasharray="strokeDasharray" :stroke-dashoffset="strokeDashoffset")
 
     g(:transform="`translate(${currentPos.x - triangleSize.width / 2} ${currentPos.y - triangleSize.height / 2})`")
-      g(:transform="`rotate(${currentPos.deg}, ${triangleSize.width / 2}, ${triangleSize.height / 2})`" fill="#FD693C" stroke="#FFFFFF")
+      g(:transform="`rotate(${currentPos.deg}, ${triangleSize.width / 2}, ${triangleSize.height / 2})`" :fill="triggerColor" stroke="#FFFFFF")
         path(d="M6.59134999999992,1.2714129999999955 C6.941420000000107,1.1138809999999921 7.351589999999987,1.0898970000000077 7.738880000000108,1.2367989999999907 C8.140239999999949,1.3890409999999918 8.457149999999956,1.7059480000000065 8.609390000000076,2.1073140000000024 L8.609390000000076,2.1073140000000024 L12.918629999999894,13.468018999999998 C13.06553000000008,13.85530700000001 13.04153999999994,14.26547500000001 12.88400999999999,14.615546999999992 C12.726480000000038,14.965619000000004 12.435400000000072,15.255594000000002 12.048109999999951,15.402496000000014 C11.87815999999998,15.466961999999995 11.69789999999989,15.5 11.516129999999976,15.5 L11.516129999999976,15.5 L2.897660000000087,15.5 C2.483449999999948,15.5 2.108449999999948,15.332107000000008 1.836999999999989,15.060660000000013 C1.565560000000005,14.789213999999987 1.3976600000000872,14.414213999999987 1.3976600000000872,14 C1.3976600000000872,13.81823 1.430699999999888,13.637972999999988 1.4951699999999164,13.468018999999998 L1.4951699999999164,13.468018999999998 L5.804399999999987,2.1073140000000024 C5.9512999999999465,1.7200249999999926 6.241279999999961,1.4289449999999988 6.59134999999992,1.2714129999999955 Z")
 
   .test-plan-info
-    .test-plan-name {{ controller.testData?.name || '暂未开始' }}
+    .test-plan-name {{ controller.testData?.name || 'Not Running' }}
     .test-plan-txn
       .test-plan-txn--current {{ formattedCurrentTxn }}
       span /
       .test-plan-txn--total {{ formattedTotalTxn }}
     .test-plan-elapsed-time
-      .test-plan-elapsed-time__text {{ formattedTimeCost }}
-      .test-plan-elapsed-time__icon
+      span.test-plan-elapsed-time__text {{ formattedTimeCost }}
+      i.iconfont.icon-jishi.test-plan-elapsed-time__icon
     .test-plan-estimated-time Estimated Time: {{ formattedTimeEstimated }}
   .test-plan-config-btn(@click="openConfig", :class="{ breathe: !controller.running }")
     .test-plan-config-btn__text Config
-    .test-plan-config-btn__icon
+    CaretRightOutlined.test-plan-config-btn__icon
 </template>
 
 <script>
@@ -34,9 +34,13 @@ import { computed, defineComponent, ref, watch } from 'vue'
 import roadCoordinates from '@/assets/road-coordinates.json'
 import { controller } from '../composition/controller.js'
 import { formatNumWithUnit, formatTime } from '../composition/util.js'
+import { CaretRightOutlined } from '@ant-design/icons-vue'
 
 export default defineComponent({
   name: 'RoadMap',
+  components: {
+    CaretRightOutlined
+  },
   setup: (props, { emit }) => {
     const startPos = roadCoordinates[0]
     const triangleSize = { width: 14, height: 16 }
@@ -102,6 +106,11 @@ export default defineComponent({
       return formatTime(controller.timeCost)
     })
 
+    const triggerColor = computed(() => {
+      if (controller.tps >= 100000) return '#CD401A'
+      return '#FD693C'
+    })
+
     const formattedTimeEstimated = computed(() => {
       if (!controller.testData) return '00:00.00'
       return formatTime(controller.testData.estimated)
@@ -125,6 +134,7 @@ export default defineComponent({
       formattedTotalTxn,
       formattedTimeCost,
       formattedTimeEstimated,
+      triggerColor,
       openConfig
     }
   },
@@ -138,17 +148,19 @@ export default defineComponent({
     flex-direction: column;
     color: #fff;
     align-items: flex-end;
-    padding-right: 20px;
+    padding-right: 5px;
     font-family: Helvetica, Helvetica-Regular;
     .test-plan-name {
       font-size: 30px;
-      font-weight: bold;
+      line-height: 42px;
+      margin-top: -6px;
     }
     .test-plan-txn, .test-plan-estimated-time {
       font-size: 16px;
       display: flex;
       font-style: italic;
       color: rgba(255, 255, 255, 0.45);
+      line-height: 19px;
       span {
         margin: 0 4px;
         color: #fff;
@@ -161,29 +173,46 @@ export default defineComponent({
       font-size: 34px;
       font-weight: bold;
       font-style: italic;
+      line-height: 42px;
+      margin-top: 14px;
+      display: flex;
+      align-items: center;
+      &__icon {
+        font-weight: normal;;
+        display: inline-block;
+        margin: -2px 1px 0 11px;
+        font-size: 24px;
+        line-height: 24px;
+      }
     }
   }
   .test-plan-config-btn {
     position: fixed;
     right: 0;
-    padding: 12px 45px 10px 16px;
-    margin-top: 40px;
+    padding: 12px 46px 10px 16px;
+    margin-top: 38px;
     color: #fff;
     font-weight: bold;
     font-style: italic;
     font-size: 20px;
+    line-height: 24px;
     border-radius: 8px 0px 0px 8px;
     user-select: none;
     cursor: pointer;
     background: linear-gradient(90deg,rgba(0, 0, 0, 0.80) 46%,  rgba(0, 0, 0, 0.00));
+    display: flex;
+    align-items: center;
     &.breathe {
       box-shadow: 0px 0px 20px 0px rgba(179, 195, 255, 0.50);
       .test-plan-config-btn__text {
         background-image:-webkit-linear-gradient(left, #FFCB00, #F08B00); 
         background-clip: text;
-        -webkit-background-clip: text; 
+        -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
       }
+    }
+    &__icon {
+      margin-left: 35px;
     }
   }
 }
