db01fe9546d6b93322ddcda87d9d56190baec356
weihaopeng <Weihp16@gmail.com>
2023-03-25 10:03:44 +0800

    add hash and mint img animation while star field running.

--- src/pages/Benchmark/components/star-field.vue
+++ src/pages/Benchmark/components/star-field.vue
@@ -1,11 +1,20 @@
 <template lang="pug">
 .star-field-container(ref="starFieldCvs" :class="starFieldStateCls")
+.hash-move-window(v-show="hashFly")
+  .hash-move(v-for="hashObj in hashList" :key="hashObj.hash" :style="{ transform: hashObj.transform, opacity: hashObj.opacity }") {{ hashObj.hash }}
+  .block-move(v-for="blockObj in blockList" :key="blockObj.hash" :style="{ transform: blockObj.transform, opacity: blockObj.opacity }")
+    img(:src="blockObj.status === 'minting' ? mintingImg : confirmedImg" :class="blockObj.status")
+  img(:src="mintingImg" style="position: fixed; top: -1000px;")
+  img(:src="confirmedImg" style="position: fixed; top: -1000px;")
 </template>
 
 <script>
-import { computed, defineComponent, onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { computed, defineComponent, onBeforeUnmount, onMounted, ref, watch, nextTick } from 'vue'
 import { StarField } from '../composition/star-field.js'
 import { controller } from '../composition/controller.js'
+import { getAddressKeyString, randomBetween } from '../composition/util'
+import mintingImg from '@/assets/minting.png'
+import confirmedImg from '@/assets/confirmed.png'
 
 export default defineComponent({
   name: 'StarField',
@@ -13,6 +22,9 @@ export default defineComponent({
     const starFieldCvs = ref(null)
     const starField = new StarField()
     const needAnimate = ref(false)
+    const hashFly = ref(false)
+    const hashList = ref([])
+    const blockList = ref([])
 
     onMounted(() => {
       starField.init(starFieldCvs.value)
@@ -22,11 +34,65 @@ export default defineComponent({
       if (controller.tps >= 100000) starField.speedUp()
     })
 
+    const getOffset = () => {
+      const side = Math.random() < 0.5
+      return {
+        offsetY: randomBetween(-15, -1, 1),
+        offsetX: side ? randomBetween(200, 600, 20) : randomBetween(-600, -200, 20),
+        offsetXASide: side ? randomBetween(-600, -200, 20) : randomBetween(200, 600, 20)
+      }
+    }
+
+    const setElementPerspectivePos = (list, offsetZFrom, offsetZTo, offsetY, offsetX, rotateY, isBlock) => {
+      list.push({
+        hash: getAddressKeyString(),
+        opacity: 0,
+        transform: `translateZ(${offsetZFrom}) translateY(${offsetY}) translateX(${offsetX}) rotateY(${rotateY})`,
+        status: isBlock ? 'minting' : undefined
+      })
+      setTimeout(() => {
+        if (list[0]) {
+          list[0].transform = `translateZ(${offsetZTo}) translateY(${offsetY}) translateX(${offsetX}) rotateY(${rotateY})`
+          list[0].opacity = isBlock ? 0.65 : 0.85
+        }
+      }, 300)
+      if (isBlock) {
+        setTimeout(() => {
+          if (list[0]) list[0].status = 'confirmed'
+        }, 800)
+      }
+    }
+
+    const startFly = () => {
+      hashFly.value = true
+      const { offsetY, offsetX, offsetXASide } = getOffset()
+      const hashDirection = offsetX / Math.abs(offsetX)
+      const blockDirection = offsetXASide / Math.abs(offsetXASide)
+      setElementPerspectivePos(hashList.value, '-1500px', '500px', `${offsetY}vh`, `${offsetX}px`, `${hashDirection * 90}deg`)
+      setElementPerspectivePos(blockList.value, '-1500px', '200px', `${offsetY}vh`, `${offsetXASide}px`, `${blockDirection * 5}deg`, true)
+
+      setTimeout(() => {
+        hashList.value.splice(0, 1)
+        blockList.value.splice(0, 1)
+        if (!hashFly.value || controller.state !== 'running') return
+        startFly()
+      }, 2000)
+    }
+
+    const stopFly = () => {
+      hashFly.value = false
+    }
+
     watch(
       () => controller.tps,
       (val) => {
-        if (val >= 100000) starField.speedUp()
-        else starField.speedDown()
+        if (val >= 100000) {
+          starField.speedUp()
+          if (!hashFly.value) startFly()
+        } else {
+          starField.speedDown()
+          stopFly()
+        }
       }
     )
     
@@ -38,6 +104,7 @@ export default defineComponent({
       } else {
         starField.stop()
         needAnimate.value = false
+        stopFly()
       }
     })
 
@@ -52,7 +119,12 @@ export default defineComponent({
     return {
       starFieldCvs,
       controller,
-      starFieldStateCls
+      starFieldStateCls,
+      hashList,
+      hashFly,
+      blockList,
+      mintingImg,
+      confirmedImg
     }
   },
 })
@@ -96,6 +168,37 @@ export default defineComponent({
   }
   &.completed {
     opacity: 0;
+  }
+}
+.hash-move-window {
+  position: absolute;
+  width: 100%;
+  height: 50%;
+  top: 0;
+  color: #fff;
+  font-size: 40px;
+  font-weight: bold;
+  perspective-origin: center bottom;
+  perspective: 10vw;
+  .hash-move, .block-move {
+    position: absolute;
+    top: 40%;
+    left: calc(50% - 450px);
+    width: 900px;
+    text-align: center;
+    // transform: translateZ(-500px) translateY(-15vh) translateX(40vw) rotateY(-90deg);
+    opacity: 0;
+    transition: all 1.5s;
+  }
+  .block-move {
+    height: 200px;
+    width: 200px;
+    left: calc(50% - 150px);
+    z-index: 999;
+    transition: all 1.5s, opacity 0.5s;
+    img {
+      width: 100%;
+    }
   }
 }
 </style>
